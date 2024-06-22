e51c8742462438e6a3f2ca2fb1465ba279b46d64
Vamsi-allam <134683978+Vamsi-allam@users.noreply.github.com>
2024-06-22 14:13:34 +0530

    604 Elections in Chefland.java

--- 604 Elections in Chefland.java
+++ 604 Elections in Chefland.java
@@ -7,6 +7,21 @@ class Codechef
 	public static void main (String[] args) throws java.lang.Exception
 	{
 		// your code goes here
-
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        while(n-->0){
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            int count= 0;
+            int arr[] = new int[a];
+            for(int i = 0;i<arr.length;i++)
+            {
+                arr[i] = sc.nextInt();
+                if(arr[i]>=b){
+                    count++;
+                }   
+            }
+            System.out.println(count);
+        }
 	}
 }
